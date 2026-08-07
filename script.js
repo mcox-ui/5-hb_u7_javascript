3fd797b545da99e5e5bdbcf959198f2225343db4
Secundaria 10 <secundaria10@example.com>
2026-08-07 10:58:15 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log("Hola JavaScript!");
+
+let dineroEnLaCuenta = 100;
+let nuevoDeposito = 1200;
+let comisionBancaria = 0.05;
+
+console.log("La comisión bancaria es: $" + (dineroEnLaCuenta + nuevoDeposito) * comisionBancaria)
+
+// alert("Hola, ¡bienvenido a mi página!");
+
+// Distitos tipos de console
+// console.error("Error por consola");
+// console.warn("Envio de alertas y warnings")
+
+let nombre="Diego"
+// alert("Mi nombre es "+ nombre)
+
+// let miNombre = prompt("¿Cuál es tu nombre?");
+
+let valor = parseInt(prompt("Ingrese un valor numerico"));
+let precio = parseFloat(prompt("Ingrese un precio"));
---
+++ template.js
@@ -0,0 +1,18 @@
+let nombre = "Ana";
+let edad = 25;
+
+let saludo = "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+
+let saludo2 = `Hola, me llamo ${nombre} y tengo ${edad} años.`; 
+
+
+console.log(saludo);
+alert(saludo);
+
+console.log(saludo2);
+alert(saludo2);
+
+let poema = `Las rosas son rojas,
+las violetas son azules.`;
+
+console.log(poema)
