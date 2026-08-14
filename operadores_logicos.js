342195638b653f34e67d53be2ae2f06e87a46227
Secundaria 10 <secundaria10@example.com>
2026-08-14 10:22:58 -0300

    Operadores logicos, Ternarios, desafio 🚀¿Puedo Jubilarme?

---
+++ operadores_logicos.js
@@ -0,0 +1,14 @@
+// let numeroDeTragos = 1
+
+// 3 > 2 ? alert("3 es mayor que 2") : alert("3 no es mayor que 2");
+
+// numeroDeTragos > 0 ? alert("Usted no puede manejar.") : alert("Nos alegra que sea un conductor responsable.");
+
+let nombre = prompt("¿Cuál es tu nombre?.");
+
+let response = nombre ? ("Respuesta recibida.") : ("No recibimos tu respuesta. Por favor, decime cuál es tu nombre.");
+
+alert(response);
+console.log(response);
+
+
